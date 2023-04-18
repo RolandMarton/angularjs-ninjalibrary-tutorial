d86db1ac12e29b6359df87a9587f66a6763dd320
RolandMarton <roland111187@gmail.com>
2023-04-18 19:57:18 +0200

    Finish the fundamentals of AngularJS

---
+++ app/app.js
@@ -0,0 +1,91 @@
+var myApp = angular.module("myApp", ["ngRoute", "ngAnimate"]);
+
+myApp.config([
+  "$routeProvider",
+  "$locationProvider",
+  function ($routeProvider, $locationProvider) {
+    
+    $locationProvider.html5Mode(true);
+
+    $routeProvider
+      .when("/home", {
+        templateUrl: "views/home.html",
+        controller: "myController",
+      })
+      .when("/contact", {
+        templateUrl: "views/contact.html",
+        controller: "ContactController",
+      })
+      .when("/contact-success", {
+        templateUrl: "views/contact-success.html",
+        controller: "ContactController",
+      })
+      .when("/directory", {
+        templateUrl: "views/directory.html",
+        controller: "myController",
+      })
+      .otherwise({
+        redirectTo: "/home",
+      });
+  },
+]);
+
+myApp.directive("randomNinja", [
+  function () {
+    return {
+      restrict: "E",
+      scope: {
+        ninjas: "=",
+        title: "=",
+      },
+      templateUrl: "views/random.html",
+      transclude: true,
+      replace: true,
+      controller: function ($scope) {
+        $scope.random = Math.floor(Math.random() * 4);
+      },
+    };
+  },
+]);
+
+myApp.controller("myController", [
+  "$scope",
+  "$http",
+  function ($scope, $http) {
+    $scope.removeNinja = function (ninja) {
+      var removedNinja = $scope.ninjas.indexOf(ninja);
+      $scope.ninjas.splice(removedNinja, 1);
+    };
+
+    $scope.addNinja = function () {
+      $scope.ninjas.push({
+        name: $scope.newninja.name,
+        belt: $scope.newninja.belt,
+        rate: parseInt($scope.newninja.rate),
+        available: true,
+      });
+
+      $scope.newninja.name = "";
+      $scope.newninja.belt = "";
+      $scope.newninja.rate = "";
+    };
+
+    $scope.removeAll = function () {
+      $scope.ninjas = [];
+    };
+
+    $http.get("data/ninjas.json").then(function (response) {
+      $scope.ninjas = response.data;
+    });
+  },
+]);
+
+myApp.controller("ContactController", [
+  "$scope",
+  "$location",
+  function ($scope, $location) {
+    $scope.sendMessage = function () {
+      $location.path("/contact-success");
+    };
+  },
+]);
